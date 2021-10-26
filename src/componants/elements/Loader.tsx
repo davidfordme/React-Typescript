d216200c1211bee047cb9f9ff904e8dd1b283fed
David Ford <d.ford@digital-velocity.co.uk>
2021-10-26 12:35:04 +0100

    Added a little cute animation to the loading text

--- src/componants/elements/Loader.tsx
+++ src/componants/elements/Loader.tsx
@@ -1,15 +1,27 @@
 import './../../media/scss/elements/loader.scss';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { useGlobalContext } from '../../Context';
 
 const Loader = () => {
 
+    const loadingTextDefault:string = 'Preparing for launch';
+    const [ loadingText, setLoadingText ] = useState<string>(loadingTextDefault);
     const { isLoading } = useGlobalContext();
 
+    useEffect(() => {
+        setTimeout(() => {
+            const numberOfDots = loadingText.split('.').length - 1;
+            const newLoading = ((numberOfDots < 3) ? loadingText + '.' : loadingTextDefault);
+            setLoadingText(newLoading);
+        }, 500);
+    }, [loadingText]);
+    
     return (
-        <div className={ isLoading ? 'loader in' : 'loader out' }></div>
+        <div className={ isLoading ? 'loader in' : 'loader out' }>
+            <div className="inner">{ loadingText }</div>
+        </div>
     )
 }
 
